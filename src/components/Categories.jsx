e84859d8f7592ebce976380d87cb96c3cb7e0461
Manish Gupta <manishgupta724872@gmail.com>
2024-08-14 19:07:47 +0530

    categories section complete

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,9 +4,18 @@ import { HiOutlineArrowSmLeft } from "react-icons/hi";
 
 const Categories = () => {
   const [categories, setCategory] = useState([]);
+  const [slide, setSlide] = useState(0);
+
+  const nextSlide = () => {
+    if (categories.length - 5 == slide) return false;
+    setSlide(slide + 2);
+  };
+  const prevSlide = () => {
+    setSlide(slide - 2);
+  };
 
   const fetchCategories = async () => {
-    const response = await fetch("");
+    const response = await fetch("http://localhost:5000/category");
     const data = await response.json();
     setCategory(data);
   };
@@ -16,17 +25,37 @@ const Categories = () => {
 
   return (
     <div className="w-[1200px] mx-auto">
-      <div className="flex justify-between items-center my-5 ">
-        <div className="text-[25px] font-bold">What's on your mind?</div>
+      <div className="flex justify-between items-center my-3 ">
+        <div className="text-[20px] font-bold">What's on your mind?</div>
         <div className="flex">
-          <div className="flex justify-center items-center cursor-pointer rounded-full bg-[#e2e2e7] w-[30px] h-[30px] mx-2">
-            <HiOutlineArrowSmRight />
+          <div
+            onClick={prevSlide}
+            className="flex justify-center items-center cursor-pointer rounded-full bg-[#e2e2e7] w-[30px] h-[30px] mx-2"
+          >
+            <HiOutlineArrowSmLeft />
           </div>
-          <div className="flex justify-center items-center cursor-pointer rounded-full bg-[#e2e2e7] w-[30px] h-[30px] mx-2">
+          <div
+            onClick={nextSlide}
+            className="flex justify-center items-center cursor-pointer rounded-full bg-[#e2e2e7] w-[30px] h-[30px] mx-2"
+          >
             <HiOutlineArrowSmRight />
           </div>
         </div>
       </div>
+      <div className="flex overflow-hidden">
+        {categories.map((cat, index) => {
+          return (
+            <div
+              style={{ transform: `translateX(-${slide * 100}%)` }}
+              key={index}
+              className="w-[200px] shrink-0 duration-500"
+            >
+              <img src={cat.image} alt="" />
+            </div>
+          );
+        })}
+      </div>
+      <hr className="border-[1px]  my-10" />
     </div>
   );
 };
